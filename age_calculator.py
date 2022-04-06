a7acabd3c8bb0832384ecd099ec4ba8b7bed18a7
abdullah-alkhareji <outsiderandco@gmail.com>
2022-04-06 13:06:31 +0300

    build the functions

--- age_calculator.py
+++ age_calculator.py
@@ -1,18 +1,41 @@
-from datetime import date
+from datetime import date, datetime
+from webbrowser import get
 
 
 def get_dob():
     # write code here
+	year=input("Enter the Year of bitrh: ")
+	mounth=input("Enter the Year of bitrh: ")
+	day=input("Enter the Year of bitrh: ")
+
+	if year.isdigit() == False:
+		print("Year is invaled")
+		return
+	elif mounth.isdigit() == False:
+		print("Month is invaled")
+		return
+	elif day.isdigit() == False:
+		print("Day is invaled")
+		return
+	else:
+		return date(int(year),int(mounth),int(day))
 	...
 
 
 def get_age(dob):
     # write code here
+	today = date.today()
+	age = today - dob
+	return age.days // 365
 	...
 
 
 def main():
 	# write code here
+	dob = get_dob()
+	age = get_age(dob)
+	print(f"Your age is {age} years old")
+
 	...
 
 
